3d227f0da346f3e78c7fbbd3abd93f71c7756f3c
tradetrovehub95@gmail.com <tradetrovehub95@gmail.com>
2024-05-29 09:04:49 +0000

    Edited register_from

--- server_code/register_from.py
+++ server_code/register_from.py
@@ -7,20 +7,10 @@ import anvil.server
 @anvil.server.callable
 def register_form(username,email,password):
   # Get the form data
-   anvil.email.send(to="noreply@gmail.com", # Change this to your email address!
-                   subject=f"register from {username}",
-                   text=f""" 
-   A new person has filled out the register form!
-
-   Name: {username}
-   Email address: {email}
-   password:
-   {password}
-   """)
+   
    app_tables.register.send_row(
     username=username, 
     email=email, 
-    password=password, 
-    
+    password=password 
   )
  
