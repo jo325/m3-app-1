cea3ca162b3b48af30272f649e8e024f80807064
tradetrovehub95@gmail.com <tradetrovehub95@gmail.com>
2024-06-04 05:18:42 +0000

    Created order_syst.home_menu, order_syst.special and OIP.jpeg; Edited order_syst; Deleted order_syst.Form3 and order_syst.Form1

--- client_code/order_syst/home_menu/__init__.py
+++ client_code/order_syst/home_menu/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form3Template
+from ._anvil_designer import home_menuTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,7 +6,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form3(Form3Template):
+class home_menu(home_menuTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
