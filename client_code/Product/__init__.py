a3bde87cbfb2a0b0d82bf74c21b166e96ac607c2
tradetrovehub95@gmail.com <tradetrovehub95@gmail.com>
2024-06-04 07:30:37 +0000

    Created Product and Product.ItemTemplate1; Deleted products and products.ItemTemplate1

--- client_code/Product/__init__.py
+++ client_code/Product/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import productsTemplate
+from ._anvil_designer import ProductTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,7 +6,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class products(productsTemplate):
+class Product(ProductTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
