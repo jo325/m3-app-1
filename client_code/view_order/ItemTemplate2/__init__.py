ae7307c8b5324c8ab13eab7e248f395b65ea995e
tradetrovehub95@gmail.com <tradetrovehub95@gmail.com>
2024-06-05 13:25:31 +0000

    Edited view_order.ItemTemplate2 and view_order

--- client_code/view_order/ItemTemplate2/__init__.py
+++ client_code/view_order/ItemTemplate2/__init__.py
@@ -12,3 +12,9 @@ class ItemTemplate2(ItemTemplate2Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def delete_button_click(self, **event_args):
+    get_open_form().view_items.remove(self.item)
+    get_open_form().view_link_click()
+    """This method is called when the button is clicked"""
+    
