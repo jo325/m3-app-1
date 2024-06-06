2085e83649c93dc4a295f43cccc314a7b5fece6d
tradetrovehub95@gmail.com <tradetrovehub95@gmail.com>
2024-06-06 18:04:57 +0000

    Edited view_order.ItemTemplate2, Product, settings and order_system.maincourse; Deleted custom package dependencies

--- client_code/order_system/maincourse/__init__.py
+++ client_code/order_system/maincourse/__init__.py
@@ -4,7 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..Product import Product
+from ...Product import Product
 
 class maincourse(maincourseTemplate):
   def __init__(self, **properties):
