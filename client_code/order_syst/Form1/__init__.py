7ef1908a4734cc32f6d1bc8f2a944256b63db184
tradetrovehub95@gmail.com <tradetrovehub95@gmail.com>
2024-06-06 11:27:20 +0000

    Created order_syst.Form1; Deleted order_syst.staters_v and order_syst.main_course1

--- client_code/order_syst/Form1/__init__.py
+++ client_code/order_syst/Form1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import staters_vTemplate
+from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,7 +6,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class staters_v(staters_vTemplate):
+class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
