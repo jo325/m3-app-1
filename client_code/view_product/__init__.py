0c5802d40735cc8eecee28a31afec05a6bb7a954
tradetrovehub95@gmail.com <tradetrovehub95@gmail.com>
2024-06-08 09:21:50 +0000

    Edited view_order and view_product

--- client_code/view_product/__init__.py
+++ client_code/view_product/__init__.py
@@ -4,7 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from ..Product import Product
 
 class view_product(view_productTemplate):
   def __init__(self, **properties):
