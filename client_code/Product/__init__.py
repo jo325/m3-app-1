38a9c47798ad9e1271818970638143049e3597bf
tradetrovehub95@gmail.com <tradetrovehub95@gmail.com>
2024-06-11 16:51:18 +0000

    Edited Product, settings, view_order, view_order.ItemTemplate1 and 1 more

--- client_code/Product/__init__.py
+++ client_code/Product/__init__.py
@@ -4,7 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from ..view_order import view_order
 
 
 class Product(ProductTemplate):
@@ -12,17 +12,11 @@ class Product(ProductTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.item = None
+    self.items = []
     self.counter_value = 0
     self.counter_label.text = str(self.counter_value)
-
-  def set_item(self, item):
-    """Set the item details"""
-    self.item = item
-    self.label_name.text = item['name']
-    self.label_price.text = f"${item['price2']}"
-    self.counter_label.text = str(self.counter_value)
-
+   
+  
     # Any code you write here will run before the form opens.
 
   def increment_button_click(self, **event_args):
@@ -37,19 +31,20 @@ class Product(ProductTemplate):
     # Save the new counter value to the database
 
   def add_button_click(self, **event_args):
-   
-    card_data = {
-            'name': self.label_1,
-            'price': self.label_2,
-            'counter': self.counter_value
+   name0 = self.name2
+   price01 = self.price2.text
+   counter23 = int(self.counter_value)
+    
+   card_data = {
+            'name': name0,
+            'price': price01,
+            'counter': counter23
         }
-    self.add_to_cart(card_data)
+   self.items.append(card_data)
 
-  def add_to_cart(self, card_data):
-        # This function should be implemented to store the data
-        # and update the view_cart page
-    get_open_form().add_to_cart(card_data)
-    
+  def add_button_show(self, **event_args):
+   display_form=view_order(items=self.items)
+   get_open_form().column_panel_1.add_component(display_form)
    
-    
-   
+
+ 
--- client_code/view_order/__init__.py
+++ client_code/view_order/__init__.py
@@ -4,21 +4,19 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from ..Product import ProductTemplate
 
 class view_order(view_orderTemplate):
-  def __init__(self, **properties):
+  def __init__(self,items=None, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.cart_items = []
-    
+    self.repeating_panel_1.item_template=ProductTemplate
+    if items:
+            self.repeating_panel.items = items
 
-  def add_to_cart(self, card_data):
-        self.cart_items.append(card_data)
-        self.update_cart_view()
+  
 
-  def update_cart_view(self):
-        self.view_cart_repeating_panel.items = self.cart_items
+  
 
     
 
