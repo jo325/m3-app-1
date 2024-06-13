03fec364c8cb147243dbaf2370778366b09030a6
tradetrovehub95@gmail.com <tradetrovehub95@gmail.com>
2024-06-13 17:27:31 +0000

    Edited view.ItemTemplate1, view_product and view; Deleted view.RowTemplate1

--- client_code/view/__init__.py
+++ client_code/view/__init__.py
@@ -8,14 +8,19 @@ from ..view_product import view_product
 
 class view(viewTemplate):
   def __init__(self,temp_storage=None, **properties):
+    self.item['name1']= temp_storage['name']
+    self.item['price_2']= temp_storage['price']
+    self.item['quantity1']= temp_storage['quantity']
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.temp_storage = temp_storage 
+   
+    if not self.temp_storage:
+      self.empty_cart_panel.visible = True
+      self.column_panel_2.visible = False
+      
     self.repeating_panel_1.items = self.temp_storage
-
-    View = self.temp_storage
-    for i in View:
-      self.cart_panel_1.add_component(view_product(temp_storage=i))
+   
 
   def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
