5790a48240fe611747217ae1fe9f687b1345a3b2
tradetrovehub95@gmail.com <tradetrovehub95@gmail.com>
2024-06-15 07:41:17 +0000

    Created MainPage.view_product; Edited view.ItemTemplate1 and settings; Deleted view_product

--- client_code/view/ItemTemplate1/__init__.py
+++ client_code/view/ItemTemplate1/__init__.py
@@ -12,3 +12,7 @@ class ItemTemplate1(ItemTemplate1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def delete_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
